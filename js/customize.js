26f511dee521fcd1b4ade7adf6809aa3a149e091
Lize Wu <retniw72@gmail.com>
2022-11-04 15:38:15 +0800

    2022-1104 cp、lp、cp、404 、form表單上傳

--- js/customize.js
+++ js/customize.js
@@ -19,6 +19,32 @@ $(function () {
         $('.megamenu').slideUp(300);
     });
 
+    /*-----------------------------------*/
+    /////////////modal設定/////////////
+    /*-----------------------------------*/
+    $(function () {
+        $("#modal1").hide(); //先隱藏視窗
+        $(".modal").after('<div class="modal_overlay"></div>'); //新增透明底
+        $(".modal").prepend('<button type="button" class="close">關閉</button>'); //新增關閉按鈕
+        $(".modal_overlay").hide(); //隱藏透明底
+        //按鈕動作
+        $("#openModal").click(function (e) {
+            $(".modal_overlay").fadeIn(100);
+            $(".modal").fadeIn(100);
+            $("body").addClass("noscroll");
+            e.preventDefault();
+        });
+        //關閉function
+        function closeModal() {
+            $("#modal1").hide();
+            $(".modal_overlay").hide();
+            $("body").removeClass("noscroll");
+        }
+        //點選關閉按鈕及透明底都可關閉
+        $(".modal_overlay").click(closeModal);
+        $(".modal .close").click(closeModal);
+    });
+
     // 首頁輪播
     $('.mpSlider').slick({
         mobileFirst: true,
@@ -39,41 +65,41 @@ $(function () {
         }
     });
     // 廣告輪播
-    $('.adSlider').slick({
-        mobileFirst: true,
-        dots: false,
-        infinite: true,
-        speed: 300,
-        slidesToShow: 2,
-        slidesToScroll: 1,
-        autoplay: true,
-        arrow: true,
-        lazyLoaded: true,
-        lazyLoad: 'ondemand',
-        ease: 'ease',
-        responsive: [{
-            breakpoint: 1200,
-            settings: {
-                slidesToShow: 5,
-                slidesToScroll: 1,
-                arrows: true
-            }
-        }, {
-            breakpoint: 768,
-            settings: {
-                slidesToShow: 4,
-                slidesToScroll: 1,
-                arrows: true
-            }
-        }, {
-            breakpoint: 575,
-            settings: {
-                slidesToShow: 3,
-                slidesToScroll: 1,
-                arrows: true
-            }
-        }]
-    });
+    // $('.adSlider').slick({
+    //     mobileFirst: true,
+    //     dots: false,
+    //     infinite: true,
+    //     speed: 300,
+    //     slidesToShow: 2,
+    //     slidesToScroll: 1,
+    //     autoplay: true,
+    //     arrow: true,
+    //     lazyLoaded: true,
+    //     lazyLoad: 'ondemand',
+    //     ease: 'ease',
+    //     responsive: [{
+    //         breakpoint: 1200,
+    //         settings: {
+    //             slidesToShow: 5,
+    //             slidesToScroll: 1,
+    //             arrows: true
+    //         }
+    //     }, {
+    //         breakpoint: 768,
+    //         settings: {
+    //             slidesToShow: 4,
+    //             slidesToScroll: 1,
+    //             arrows: true
+    //         }
+    //     }, {
+    //         breakpoint: 575,
+    //         settings: {
+    //             slidesToShow: 3,
+    //             slidesToScroll: 1,
+    //             arrows: true
+    //         }
+    //     }]
+    // });
     //燈箱slick+lightBox組合
     $('.cp_slider').slick({
         dots: true,
